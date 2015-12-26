086db8ff2f3e2a1eff35a210dee2d16548196c46
Barry Wright <wright.barry@gmx.com>
2015-12-26 13:27:45 +0000

    changed graphics image interface to take int resource id instead of file url

--- mother/src/main/java/uk/ac/uea/nostromo/mother/implementation/Graphics.java
+++ mother/src/main/java/uk/ac/uea/nostromo/mother/implementation/Graphics.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
@@ -13,6 +14,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -42,12 +44,22 @@ public class Graphics {
 
     /**
 	 * Returns an ImageView for the specified image in the format specified.
-	 * @param fileName The filename of the image asset to be loaded
+	 * @param resourceId The filename of the image asset to be loaded
 	 * @return An ImageView object for the image
 	 * @exception RuntimeException
 	 * @since	v1.0.0-alpha+20151204
 	 */
-    public ImageView newImage(String fileName) {
-		return new ImageView(context);
+    public ImageView newImage(int resourceId) {
+        ImageView view = new ImageView(context);
+
+        try {
+            Resources res = context.getResources();
+            Drawable drawable = res.getDrawable(resourceId);
+            view.setImageDrawable(drawable);
+        }
+        catch (Exception ex){
+            System.out.print("test " + ex.getMessage());
+        }
+        return view;
     }
 }
