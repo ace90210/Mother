940fff10a7fa10218e1161c102f455610dd3b87f
Barry Wright <wright.barry@gmx.com>
2016-01-02 01:05:15 +0000

    added graphics class updates to create text view, google maps and linear layouts

--- mother/src/main/java/uk/ac/uea/nostromo/mother/implementation/Graphics.java
+++ mother/src/main/java/uk/ac/uea/nostromo/mother/implementation/Graphics.java
@@ -1,9 +1,16 @@
 package uk.ac.uea.nostromo.mother.implementation;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.MapFragment;
 
 /**
  * Handles the drawing to the Canvas(being displayed on android screen) with multiple elements
@@ -48,5 +55,43 @@ public class Graphics {
             System.out.print("test " + ex.getMessage());
         }
         return view;
+    }
+
+    public TextView newTextView(String defaultContent, boolean multiLine, boolean fillWidth, boolean fillHeight){
+        TextView tv = new TextView(context);
+        tv.setText(defaultContent);
+        LinearLayout.LayoutParams layout = new LinearLayout.LayoutParams(
+                fillWidth ? LinearLayout.LayoutParams.MATCH_PARENT : LinearLayout.LayoutParams.WRAP_CONTENT,
+                fillHeight ? LinearLayout.LayoutParams.MATCH_PARENT : LinearLayout.LayoutParams.MATCH_PARENT
+        );
+
+        if(!multiLine){
+            tv.setMaxLines(1);
+        }
+        tv.setLayoutParams(layout);
+        return tv;
+    }
+
+    public LinearLayout newLinearLayout(){
+        LinearLayout ll = new LinearLayout(context);
+        ll.setId(View.generateViewId());
+        return ll;
+    }
+
+    public LinearLayout newLinearLayout(int width, int height){
+        LinearLayout ll = newLinearLayout();
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width, height);
+
+        ll.setLayoutParams(lp);
+
+        return ll;
+    }
+
+    public MapFragment newMap(Activity act, int containerID){
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction = act.getFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerID, mMapFragment);
+        fragmentTransaction.commit();
+        return mMapFragment;
     }
 }
